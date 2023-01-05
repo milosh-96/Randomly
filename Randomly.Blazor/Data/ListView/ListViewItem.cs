ebb6899794855f662381494ffb3e3d85813710e3
Miloš Jovanović <milosh-96@outlook.com>
2023-01-06 00:51:31 +0100

    Add project files.

---
+++ Randomly.Blazor/Data/ListView/ListViewItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Randomly.Blazor.Data.ListView
+{
+    public class ListViewItem
+    {
+        public string Key { get; set; } = "test";
+        public string DisplayText { get; set; } = "Undefined Item";
+    }
+}
